293a5ca51f08db7b2e27c65fd3ec25c13df464c3
Sindre Trosterud <sindre.reino.trosterud@gmail.com>
2020-01-11 17:33:10 +0200

    add vscode extensions and comment out powerline

--- fish/config.fish
+++ fish/config.fish
@@ -45,8 +45,8 @@ set --global --export \
     POWERLINE_ROOT_REPO /usr/local/lib/python3.7/site-packages
 set fish_function_path \
     $fish_function_path "$POWERLINE_ROOT_REPO/powerline/bindings/fish"
-powerline-daemon --quiet
-powerline-setup
+# powerline-daemon --quiet
+# powerline-setup
 
 ############################################################################
 # Configuration for Oh-My-Fish!
