12a99cedcd8f133bc4e618c48904399e69fbec40
Sindre Trosterud <sindre.reino.trosterud@gmail.com>
2020-04-21 02:18:27 +0300

    add linkedin to email function

--- fish/functions/emails.fish
+++ fish/functions/emails.fish
@@ -1,3 +1,3 @@
 function emails
-    open https://www.gmail.com https://mail.aalto.fi https://webmail.numanconsult.com
+    open https://www.gmail.com https://mail.aalto.fi https://webmail.numanconsult.com https://linkedin.com
 end
