f16ee7c916305c2cda1772846d0ff6c55821ecc1
Sindre Trosterud <sindre.reino.trosterud@gmail.com>
2020-05-08 13:10:11 +0300

    add pomodoro function

--- fish/functions/pomodoro.fish
+++ fish/functions/pomodoro.fish
@@ -1,3 +1,3 @@
 function pomodoro
-    sleep 1500 && notification "Pomodoro over" "25 minutes have passed, you are aware"
+    fish -c 'sleep 1500 ; notification "Pomodoro over" "25 minutes have passed, you are aware"' &
 end
